0758ea7f57183f05d1e3b3af01b27083cf11d8ba
Adrian Smith <adrian@smithdev.io>
2017-02-26 18:44:35 -0800

    Finished first iteration of the permuation type

--- permutation/permutation.go
+++ permutation/permutation.go
@@ -1,20 +1,106 @@
 package permutation
 
 import (
-	"github.com/oleiade/lane"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-func (PermutationNode *node) MakeNode() *lane.PQueue {
-
+type Node struct {
+	contents []int
 }
 
-func (PermutationNode *node) Children() *lane.PQueue {
+// Constructor for Node type
+// Expects: a comma seperated string representing an arrangement of integers
+// Returns: A reference to a new node representing the passed state
+func MakeNode(arrange string) (*Node, error) {
+	// Makes a new instance of the Node type
+	node := &Node{}
 
+	// Parses the passed comma seperated string into the contents of the node
+	strSlice := strings.Split(arrange, ",") // Slice is composed of strings at this point
+	intSlice := make([]int, len(strSlice))
+
+	for i, element := range strSlice {
+		intElement, err := strconv.Atoi(element)
+		if err != nil {
+			err = fmt.Errorf("Failure to parse given string (%s). Error: %v", element, err)
+			return nil, err
+		} else {
+			intSlice[i] = intElement
+		}
+	}
+
+	node.contents = intSlice
+
+	// Returns a reference to the node
+	return node, nil
 }
 
-func (PermutationNode *node) IsGoal() bool {
+// Gets the children of a node
+// Expects: a valid rootNode
+// Returns: a slice of Nodes
+func (rootNode *Node) Children() []*Node {
+	childrenSlice := []*Node{}
+
+	// Iterate through all size 2 groupings of adjacent elements
+	for subLen := len(rootNode.contents) - 1; subLen > 2; subLen-- {
+		for i := 0; i+subLen < len(rootNode.contents)+1; i++ {
+			newNode := &Node{}
+
+			// Copy the array associated with rootNode so as not to modify the original
+			newContents := make([]int, len(rootNode.contents), (cap(rootNode.contents)))
+			copy(newContents, rootNode.contents)
+
+			// Do the rotation
+			newContents = reverse(newContents[i:subLen])
+
+			// Put the new node on the slice
+			newNode.contents = newContents
+			childrenSlice = append(childrenSlice, newNode)
+		}
+	}
+
+	return childrenSlice
 }
 
-func (PermutationNode *node) Dump() string {
+// Helper function which reverses the slice passed
+func reverse(numbers []int) []int {
+	for i, j := 0, len(numbers)-1; i < j; i, j = i+1, j-1 {
+		numbers[i], numbers[j] = numbers[j], numbers[i]
+	}
 
+	return numbers
+}
+
+// Checks if the given node is a goal node
+// Expects: A valid node
+// Returns: A boolean representing whether the node was a goal
+func (node *Node) IsGoal() bool {
+	// If we find any breakpoints return false
+	// A breakpoint in a permutation X is a position j such that X(j) + 1 ≠ X(j+1)
+	for i, element := range node.contents[:len(node.contents)-1] {
+		if element+1 != node.contents[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Gives the contents of a node in a comma seperated string
+// Expects: a valid node
+// Returns: A string of comma seperated integers
+func (node *Node) Dump() string {
+	strSlice := []string{}
+
+	// Casting contents
+	for _, elem := range node.contents {
+		s := strconv.Itoa(elem)
+		strSlice = append(strSlice, s)
+	}
+
+	dumpString := strings.Join(strSlice, ",")
+
+	return dumpString
 }
--- permutation/permutation_test.go
+++ permutation/permutation_test.go
@@ -1,41 +1,76 @@
 package permutation
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestMakeNode(t *testing.T) {
-	actual := true
-	expected := true
+	testNode, err := MakeNode("1,2,3,4,5")
 
-	if actual != expected {
-		t.Errorf("Error occured while testing Childen: '%s' != '%s'", expected, actual)
+	if err != nil {
+		t.Errorf("Fatal error while making new node: %s", err)
+	}
+
+	actual := testNode.contents
+	expected := []int{1, 2, 3, 4, 5}
+
+	if !(reflect.DeepEqual(actual, expected)) {
+		t.Errorf("Error occured while testing MakeNode: '%v' != '%v'", expected, actual)
+	}
+
+	_, err_2 := MakeNode("a; 1, 12.. a")
+
+	if err_2 == nil {
+		t.Errorf("Error occured while testing MakeNode: Bad string is not rejected")
 	}
 }
 
 func TestChildren(t *testing.T) {
-	actual := true
-	expected := true
+	testNode, _ := MakeNode("1,2,3,4,5")
 
-	if actual != expected {
-		t.Errorf("Error occured while testing Childen: '%s' != '%s'", expected, actual)
+	actual := testNode.contents
+	expected := []int{1, 2, 3, 4, 5}
+
+	if !(reflect.DeepEqual(actual, expected)) {
+		t.Errorf("Error occured while testing Childen, parent was modified: '%v' != '%v'", expected, actual)
+	}
+
+	actual_2 := testNode.Children()[0].contents
+	expected_2 := []int{4, 3, 2, 1}
+
+	if !(reflect.DeepEqual(actual_2, expected_2)) {
+		t.Errorf("Error occured while testing Childen: '%v' != '%v'", expected_2, actual_2)
 	}
 }
 
 func TestIsGoal(t *testing.T) {
-	actual := true
+	goalNode, _ := MakeNode("1,2,3,4,5")
+
+	actual := goalNode.IsGoal()
 	expected := true
 
 	if actual != expected {
-		t.Errorf("Error occured while testing IsGoal: '%s' != '%s'", expected, actual)
+		t.Errorf("Error occured while testing IsGoal: e:'%v' != a:'%v'", expected, actual)
+	}
+
+	failNode, _ := MakeNode("1,2,3,5,4")
+
+	actual_2 := failNode.IsGoal()
+	expected_2 := false
+
+	if actual_2 != expected_2 {
+		t.Errorf("Error occured while testing IsGoal: e:'%v' != a:'%v'", expected_2, actual_2)
 	}
 }
 
 func TestDump(t *testing.T) {
-	actual := true
-	expected := true
+	goalNode, _ := MakeNode("1,2,3,4,5")
+
+	actual := goalNode.Dump()
+	expected := "1,2,3,4,5"
 
 	if actual != expected {
-		t.Errorf("Error occured while testing Dump: '%s' != '%s'", expected, actual)
+		t.Errorf("Error occured while testing Dump: e:'%v' != a:'%v'", expected, actual)
 	}
 }
